3449c0558d9f6f8db3ed86307da474234906f2a0
Coralie Baudry <coralie.bdy@gmail.com>
2021-06-09 13:07:40 +0200

    API:REFACTOR:Changing  fil storage

--- Test_02/app.js
+++ Test_02/app.js
@@ -21,14 +21,12 @@ app.use( (req, res) => {
     })
 })
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
+app.use((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
 
-
+})
 
 module.exports=app
 
--- Test_02/database/readJson.js
+++ Test_02/database/readJson.js
@@ -1,9 +1,11 @@
 const csvToJson = require('convert-csv-to-json')
-const fileInputName = 'src/top-500-most-rented-movies.csv'
-const fileOutputName = 'src/top-500-most-rented-movies.json'
+const fileInputName = 'top-500-most-rented-movies.csv'
 
 csvToJson.parseSubArray('*',',').getJsonFromCsv(fileInputName)
 csvToJson.formatValueByType().getJsonFromCsv(fileInputName)
 csvToJson.utf8Encoding().getJsonFromCsv(fileInputName)
-csvToJson.generateJsonFileFromCsv(fileInputName,fileOutputName)
 
+let json = csvToJson.getJsonFromCsv(fileInputName)
+for(let i = 0; i < json.length; i ++){
+    console.log(json[i]);
+}
